23bf0e7fc6f90d29faec899cb0939e467596061e
mrogach2350 <m.rogach@gmail.com>
2017-02-17 14:19:42 -0800

    adds in youtube search package and api key to index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCVYEm0cS115IltN15h31ueQ74CJhYHdOI';
 
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+  console.log(data);
+});
 
 const App = () => {
   return (
