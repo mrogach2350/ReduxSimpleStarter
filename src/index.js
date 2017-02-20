bb7d5234e689e0de8e4992f14033a2cb627c7d0f
mrogach2350 <m.rogach@gmail.com>
2017-02-20 14:25:14 -0800

    abstracts out search function and adds lodash/debounce

--- src/index.js
+++ src/index.js
@@ -1,18 +1,46 @@
+import _ from 'lodash'
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_details';
 const API_KEY = 'AIzaSyCVYEm0cS115IltN15h31ueQ74CJhYHdOI';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-  console.log(data);
-});
-
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo : null
+    };
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
   render(){
+    const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300);
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar
+          onSearchTermChange={videoSearch} />
+
+        <VideoDetail video={this.state.selectedVideo} />
+
+        <VideoList
+          onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
       </div>
     );
   }
